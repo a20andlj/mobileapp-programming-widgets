a8fba5c2facfd583f22d34d114dbd3124df87207
Andreas <a20andlj@student.his.se>
2021-04-22 10:35:24 +0200

    Startat om med LinnearLayout o lagt till EditT o Button

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.widgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
 import android.widget.ImageView;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,9 +18,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button button = findViewById(R.id.my_button);
 
-
-
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("==>", "Tryckt skicka: ");
+            }
+        });
 
     }
 }
